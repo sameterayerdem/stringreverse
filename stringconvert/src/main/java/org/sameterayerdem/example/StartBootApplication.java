878f8f3d885cb6b0b7704f83eecff497a65a10ea
julia <julia@julia-X555LNB>
2018-07-03 19:34:08 +0300

    first

---
+++ stringconvert/src/main/java/org/sameterayerdem/example/StartBootApplication.java
@@ -0,0 +1,12 @@
+package org.sameterayerdem.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartBootApplication.class, args);
+	}
+}
