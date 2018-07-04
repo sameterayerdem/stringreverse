a1a06f114c8b1dca4c71f539a59c7ad408837b3d
julia <julia@julia-X555LNB>
2018-07-04 12:46:28 +0300

    aaaa

--- reversestring/src/main/java/org/sameterayerdem/example/Service.java
+++
@@ -1,8 +0,0 @@
-package org.sameterayerdem.example;
-
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-
-public interface Service {
-	public String convert(String value);
-}
